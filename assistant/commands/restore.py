127bbf1050428df436797a4d2c08f09dac26348f
Anton Samofal <devtosha@gmail.com>
2022-07-14 20:38:21 +0300

    Not allow to run restore command on running instance

--- assistant/commands/restore.py
+++ assistant/commands/restore.py
@@ -1,6 +1,8 @@
+from rich.text import Text
+
 from assistant import Environment
 from assistant.configs import Config
-from utils import is_mysql_running
+from utils import is_mysql_running, rprint
 
 
 class RestoreCommand:
@@ -9,4 +11,13 @@ class RestoreCommand:
         self._config = config
 
     def execute(self):
-        print(is_mysql_running())
+        if is_mysql_running():
+            message = Text.assemble(
+                ('Please restart the container with env variable: ', 'yellow3'),
+                ('XTRABACKUP_RESTORE=true', 'blue bold')
+            )
+            rprint(message)
+
+            raise RuntimeError('MySQL server needs to be shut down before restore is performed.')
+
+        rprint('Restoring...')
