e0a30e12e25fb3566618a0621793047a1a201912
Anton Samofal <devtosha@gmail.com>
2022-08-11 16:18:33 +0300

    Move clear_temp_dir to data_dir module

--- xtrabackup-assistant/assistant/assistant.py
+++ xtrabackup-assistant/assistant/assistant.py
@@ -1,9 +1,5 @@
-import os
-import shutil
-
 from common import Environment
 from configs import Config
-from constants import TEMP_DIR_PATH
 from .commands import Command, CreateCommand, RestoreCommand
 
 
@@ -18,12 +14,3 @@ class Assistant:
             CreateCommand(self._env, self._config).execute(is_upload)
         elif command is Command.RESTORE:
             RestoreCommand(self._env, self._config).execute()
-
-    @staticmethod
-    def clear_temp_dir() -> None:
-        with os.scandir(str(TEMP_DIR_PATH)) as entries:
-            for entry in entries:
-                if entry.is_dir() and not entry.is_symlink():
-                    shutil.rmtree(entry.path)
-                elif entry.name != '.gitignore':
-                    os.remove(entry.path)
